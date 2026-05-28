81a5d1ec6e690daf8ca06b4789752ef3d5156879
Max Belich <maxbelich@web.de>
2026-05-28 18:33:02 +0200

    added menu section and cards, fonts and styling

--- script/template.js
+++ script/template.js
@@ -1,7 +1,12 @@
 function getMainTemplate() {
   return `
-    <nav id="header"></nav>
+    <header id="header"></header>
     <section id="hero"></section>
+
+    <section class="app_layout">
+      <section id="menu_content"></section>
+      <aside id="basket_content"></aside>
+    </section>
   `;
 }
 
@@ -44,13 +49,51 @@ function getHeroTemplate() {
       </div>
 
       <div class="restaurant_info">
-        <h1>BurgerHouse</h1>
+        <h1>
+  <span class="burger_text">Burger</span>House
+</h1>
 
         <p>
           The best of Burgers, Pizza, and Greens, all in one great place.
         </p>
       </div>
 
+    </div>
+  `;
+}
+
+function getCategoryTemplate() {
+  return `
+    <div class="category_bar">
+      <img 
+        src="./assets/icons/burger_logo.svg" 
+        alt=""
+        class="category_icon"
+      >
+
+      <h2>Burger</h2>
+    </div>
+  `;
+}
+
+function getProductCardTemplate() {
+  return `
+    <div class="product_card">
+      <img 
+        src="./assets/imgs/veggie_mushroom_black_burger.jpg" 
+        alt=""
+        class="product_img"
+      >
+
+      <div class="product_info">
+        <h3>Veggie mushroom black burger</h3>
+        <p>Mixed green salad, Tomatoes, Edamame, Mushrooms</p>
+      </div>
+
+      <div class="product_action">
+        <span class="product_price">16,90€</span>
+        <button class="add_btn">Add to basket</button>
+      </div>
     </div>
   `;
 }
