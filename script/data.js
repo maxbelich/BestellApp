1e654916369890122c255327918564ab1d206b9f
Max Belich <maxbelich@web.de>
2026-05-28 20:30:03 +0200

    function added to show whole menu list and styling

--- script/data.js
+++ script/data.js
@@ -1,88 +1,119 @@
 const products = [
-  // BURGER
   {
     category: "Burger",
     name: "Veggie mushroom black burger",
     description: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-    price: 16.90,
+    price: 16.9,
+    img: "veggie_mushroom_black_burger.jpg",
   },
 
   {
     category: "Burger",
     name: "All meat burger",
     description: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
-    price: 15.90,
+    price: 15.9,
+    img: "all_meat_burger.jpg",
   },
 
   {
     category: "Burger",
     name: "Beef red burger",
     description: "Beef, Cheese, Tomatoes, Lettuce, Onion",
-    price: 14.90,
+    price: 14.9,
+    img: "beef_red_burger.jpg",
   },
 
   {
     category: "Burger",
     name: "Big chicken burger",
     description: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
-    price: 15.90,
+    price: 15.9,
+    img: "big_chicken_burger.jpg",
   },
 
-  // PIZZA
   {
     category: "Pizza",
     name: "Pizza Margherita",
     description: "Tomato Sauce, Mozzarella",
-    price: 11.90,
+    price: 11.9,
+    img: "pizza_margherita.jpg",
   },
 
   {
     category: "Pizza",
     name: "Pizza Chorizo",
     description: "Tomato slices, Mozzarella, Chorizo",
-    price: 13.90,
+    price: 13.9,
+    img: "pizza_chorizo.jpg",
   },
 
   {
     category: "Pizza",
     name: "Funghi",
     description: "Red onion, Olives, Button Mushrooms, Mozzarella",
-    price: 12.90,
+    price: 12.9,
+    img: "funghi.jpg",
   },
 
   {
     category: "Pizza",
     name: "Quattro Formaggi with Chicken",
-    description: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-    price: 15.90,
+    description:
+      "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+    price: 15.9,
+    img: "quattro_formaggi_with_chicken.jpg",
   },
 
-  // SALAD
   {
     category: "Salad",
     name: "Warm beef arugula salad",
-    description: "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
-    price: 16.90,
+    description:
+      "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    price: 16.9,
+    img: "warm_beef_arugula_salad.jpg",
   },
 
   {
     category: "Salad",
     name: "Mini green Salad",
     description: "Green salad, Cucumber, Carrots, Parsley, Radishes",
-    price: 7.90,
+    price: 7.9,
+    img: "mini_green_salad.jpg",
   },
 
   {
     category: "Salad",
     name: "Green Salad with sea food",
-    description: "Mixed greens, Cherry tomatoes, Red onion, Mussles, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-    price: 16.90,
+    description:
+      "Mixed greens, Cherry tomatoes, Red onion, Mussles, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    price: 16.9,
+    img: "green_salad_with_sea_food.jpg",
   },
 
   {
     category: "Salad",
     name: "Vegan green salad with tofu",
-    description: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-    price: 14.90,
+    description:
+      "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    price: 14.9,
+    img: "vegan_green_salad_with_tofu.jpg",
+  },
+];
+
+const categories = [
+  {
+    name: "Burger & Sandwiches",
+    key: "Burger",
+    icon: "burger_logo.svg",
+  },
+  {
+    name: "Pizza",
+    key: "Pizza",
+    icon: "pizza_logo.svg",
+  },
+  {
+    name: "Salad",
+    key: "Salad",
+    icon: "salad_logo.svg",
   },
 ];
--- script/script.js
+++ script/script.js
@@ -26,9 +26,22 @@ function renderHero() {
 
 function renderMenu() {
   let menuContentRef = document.getElementById("menu_content");
+  menuContentRef.innerHTML = "";
+  for (
+    let categoryIndex = 0;
+    categoryIndex < categories.length;
+    categoryIndex++
+  ) {
+    menuContentRef.innerHTML += getCategoryTemplate(categoryIndex);
+    renderProductsByCategory(categoryIndex);
+  }
+}
 
-  menuContentRef.innerHTML = `
-    ${getCategoryTemplate()}
-    ${getProductCardTemplate()}
-  `;
+function renderProductsByCategory(categoryIndex) {
+  let menuContentRef = document.getElementById("menu_content");
+  for (let productIndex = 0; productIndex < products.length; productIndex++) {
+    if (products[productIndex].category === categories[categoryIndex].key) {
+      menuContentRef.innerHTML += getProductCardTemplate(productIndex);
+    }
+  }
 }
